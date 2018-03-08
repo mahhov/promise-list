17c30148388843dbbbe520fc30c5c43abe001bdb
manukfarmers <mahhov@yahoo.com>
2018-03-08 14:37:45 -0800

    convert to js class format and add resolve & reject functions

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-function PromiseEach(promise) {
-    this.then = handler => new PromiseEach(promise.then(dataList => dataList.map(handler)));
-    this.catch = handler => new PromiseEach(promise.then(dataList => dataList.map(handler)));
-    this.toPromise = () => promise;
+class PromiseEach {
+    constructor(promise) {
+        this.promise = promise;
+    }
+
+    then(handler) {
+        return new PromiseEach(promise.then(dataList => dataList.map(handler)));
+    }
+
+    catch(handler) {
+        return new PromiseEach(promise.then(dataList => dataList.map(handler)))
+    };
+
+    toPromise() {
+        return promise
+    }
+
+    static resolve(list) {
+        return new PromiseEach(Promise.resolve(list));
+    }
+
+    static reject(list) {
+        return new PromiseEach(Promise.reject(list));
+    }
+
+    static all() {}
+
+    static race() {}
 };
