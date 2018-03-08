60f9baf206ebc6a80f889d2764fd9aed274ba373
manukfarmers <mahhov@yahoo.com>
2018-03-08 14:44:04 -0800

    fix missing this accessor in class conversion and update readme

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-class PromiseEach {
+class PromiseL {
     constructor(promise) {
         this.promise = promise;
     }
 
     then(handler) {
-        return new PromiseEach(promise.then(dataList => dataList.map(handler)));
+        return new PromiseL(this.promise.then(dataList => dataList.map(handler)));
     }
 
     catch(handler) {
-        return new PromiseEach(promise.then(dataList => dataList.map(handler)))
+        return new PromiseL(this.promise.then(dataList => dataList.map(handler)))
     };
 
     toPromise() {
-        return promise
+        return this.promise;
     }
 
     static resolve(list) {
-        return new PromiseEach(Promise.resolve(list));
+        return new PromiseL(Promise.resolve(list));
     }
 
     static reject(list) {
-        return new PromiseEach(Promise.reject(list));
+        return new PromiseL(Promise.reject(list));
     }
 
     static all() {}
--- readme.md
+++ readme.md
@@ -15,7 +15,7 @@ promise.then(numList => numList.map(num => num - 1)).then(numList => numList.map
 ### With promise-l
 
 ```
-let promisel = PromiseEach.resolve([1, 2, 3, 4]);
+let promisel = PromiseL.resolve([1, 2, 3, 4]);
 promisel.then(num => num - 1).then(num => num * 2).toPromise().then(numList => {console.log(numList)});
 // [ 0, 2, 4, 6 ]
 ```
